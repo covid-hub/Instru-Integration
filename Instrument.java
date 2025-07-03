44a50693100f104a2492a35f25b8d385847d5c5d
covid-hub <banerjeesupravo2000@gmail.com>
2025-07-04 00:32:17 +0530

    define function

--- Instrument.java
+++ Instrument.java
@@ -1,3 +1,34 @@
 //Let's connect an instrument
 
-public static void main
+package com....labvantage.sapphire.sdms.attachmenthandler
+
+import sapphire.util
+import sapphire.attachment.attachment
+import sapphire.util.StringUtil
+import sapphire.util.Array
+
+import java.io.InputStream
+import java.io.BufferReader
+import java.io.
+
+public static Instrment extends BaseAttachmentHandler 
+
+public void datahandling (Attachment attachment<String,String>) throws SapphireException {
+if (attachments.isEmpty || attachments ==Null){
+  throw new SapphireException('Instrument input file is not found')
+try {
+  for (Attachment attachment : attachments) {
+    List<map<String,String>> results=pasrseFile(attachment);
+    replicateData(results)
+    saveData(results)
+
+    for(row:results){
+      logger.info(row.toString())
+    }
+  }
+}
+catch{
+  sapphireException('Input file cauld not be parsed')
+}
+}
+}
