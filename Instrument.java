9b3acdc526beab5a51c97fe26a3809b545c604b7
covid-hub <banerjeesupravo2000@gmail.com>
2025-07-05 20:07:38 +0530

    Remove tthe header and any empty line

--- Instrument.java
+++ Instrument.java
@@ -34,18 +34,33 @@ catch{
 }
 
 public static void parseFile(Attachment input) throws SapphireException {
-//1.Identify the header
+
 Stream prFile=InputStream(input);
 Srting cnFile=convertToString(prFile);
-List<map<String,String>> grFile=prFile.straem().
+//1.Identify the header
+
+List<map<String,String>> header=cnFile.subString(0,cnFile.indexOf("/n"));//0 starting index and last index not including
+List<String> headerList=Array.straem(header).map(String::trim).collect(Collectors.toList());//Trim the header line of whitespaces ans empty strings
+
+List<map<String,String>> content=removeExtraLine(cnFile)
+if(header.size()==content.size()){}
+
 }
 
-public static void convertToString(InputStream input){
+public static Sring convertToString(InputStream input){
  try(BufferReader bf=new BufferReader(new InputStreamReader(input))) {
  return bf.lines().collect(Collectors.joining("\n"));
  } catch (SapphireException e){
   throw new SapphireException("Input file can't be read",e);
  }
+
+}
+
+public static String removeExtraLine(String ex){
+  return Array.straem(ex.split("\n"))
+  .filter(lamb->!lamb.trim().isEmpty())//removing the empty lines
+  .filter(lamb->!lamb.startsWith("Sample id"))//Removing the 1st header row
+  .collect(Collectors.toList())
 
 }
 }
